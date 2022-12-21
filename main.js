9913783e6108fd30bd7b9898f89fb71a17121a82
mehdi <mahdiridmani22@gmail.com>
2022-12-21 15:36:03 +0100

    new landing page

---
+++ main.js
@@ -0,0 +1,11 @@
+let img = document.querySelector('.img');
+let container = document.querySelector('.container');
+
+
+function phones(phone){
+    img.src = phone ; 
+}
+
+function colors(color){
+    container.style.background = color;
+}
